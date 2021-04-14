7befa35e4a66643540e4a284ba1cd72adeaec82a
Natanael Lima <taelima1997@gmail.com>
2021-04-13 23:40:06 -0300

    add initial index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+import { Radio, RadioGroup, Stack, Switch } from '@chakra-ui/react';
+
 export default function Home() {
-  return <h1>alooo</h1>;
+  const [value, setValue] = useState<string>('1');
+  return (
+    <RadioGroup onChange={setValue} value={value}>
+      <Stack direction="row">
+        <Radio value="1">First</Radio>
+        <Radio value="2">Second</Radio>
+        <Radio value="3">Third</Radio>
+        <Switch size="lg" />
+      </Stack>
+    </RadioGroup>
+  );
 }
