fff8e9c6412d4b2489b4dcba3e248799ae6d7737
Natanael Lima <taelima1997@gmail.com>
2021-04-14 23:06:23 -0300

    add singin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,64 @@
-import { useState } from 'react';
-import { Radio, RadioGroup, Stack, Switch } from '@chakra-ui/react';
+import {
+  Flex,
+  Input,
+  Button,
+  Stack,
+  FormLabel,
+  FormControl,
+} from '@chakra-ui/react';
 
 export default function Home() {
-  const [value, setValue] = useState<string>('1');
   return (
-    <RadioGroup onChange={setValue} value={value}>
-      <Stack direction="row">
-        <Radio value="1">First</Radio>
-        <Radio value="2">Second</Radio>
-        <Radio value="3">Third</Radio>
-        <Switch size="lg" />
-      </Stack>
-    </RadioGroup>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
