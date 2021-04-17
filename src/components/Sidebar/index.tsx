c8595d2dde6692ce03562395bcf2fc3d69c5ba18
Natanael Lima <taelima1997@gmail.com>
2021-04-16 23:02:36 -0300

    update sidebar with components

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Stack, Text, Link, Icon } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
+import React from 'react';
 import {
   RiContactsLine,
   RiDashboardLine,
@@ -6,48 +7,21 @@ import {
   RiInputMethodLine,
 } from 'react-icons/ri';
 
+import { NavLink } from './NavLink';
+import { NavSection } from './NavSection';
+
 export function Sidebar() {
   return (
     <Box as="aside" w="64" mr="8">
       <Stack spacing="12" align="flex-start">
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            GERAL
-          </Text>
-          <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
-              <Icon as={RiDashboardLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Dashboard
-              </Text>
-            </Link>
-            <Link display="flex" align="center">
-              <Icon as={RiContactsLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Usuários
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            AUTOMAÇÃO
-          </Text>
-          <Stack spacing="4" mt="8" align="stretch">
-            <Link display="flex" align="center">
-              <Icon as={RiInputMethodLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Formulários
-              </Text>
-            </Link>
-            <Link display="flex" align="center">
-              <Icon as={RiGitMergeLine} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                Automação
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
+        <NavSection title="GERAL">
+          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+          <NavLink icon={RiContactsLine}>Usuários</NavLink>
+        </NavSection>
+        <NavSection title="AUTOMAÇÃO">
+          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
+          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+        </NavSection>
       </Stack>
     </Box>
   );
