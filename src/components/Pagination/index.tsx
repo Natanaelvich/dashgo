79a600d591f380b4f13b29216505e43637c9f8dd
Natanael Lima <taelima1997@gmail.com>
2021-04-16 23:02:23 -0300

    update pagination with components

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
 import { Stack, Button, Box } from '@chakra-ui/react';
+import { PaginationItem } from './PaginationItem';
 
 export function Pagination() {
   return (
@@ -13,52 +14,12 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          colorScheme="pink"
-          disabled
-          _disabled={{
-            bg: 'pink.500',
-            cursor: 'default',
-          }}
-        >
-          1
-        </Button>
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: 'gray.500',
-          }}
-        >
-          2
-        </Button>
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: 'gray.500',
-          }}
-        >
-          3
-        </Button>
-        <Button
-          size="sm"
-          fontSize="xs"
-          width="4"
-          bg="gray.700"
-          _hover={{
-            bg: 'gray.500',
-          }}
-        >
-          4
-        </Button>
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+        <PaginationItem number={6} />
       </Stack>
     </Stack>
   );
