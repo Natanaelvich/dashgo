55c0f898edf2daba9cc00179d33bf1c00e9afa3d
Natanael Lima <taelima1997@gmail.com>
2021-04-16 23:07:57 -0300

    add responsiveness to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Diego Fernandes</Text>
-        <Text color="gray.300" fontSize="small">
-          diego.schell.f@gmail.com
-        </Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Diego Fernandes</Text>
+          <Text color="gray.300" fontSize="small">
+            diego.schell.f@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Diego Fernandes"
@@ -18,3 +23,7 @@ export function Profile() {
     </Flex>
   );
 }
+
+Profile.defaultProps = {
+  showProfileData: true,
+};
