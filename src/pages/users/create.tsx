a75dc4ba8f4b993e009db3ae71a20d0c5eee9a19
Natanael Lima <taelima1997@gmail.com>
2021-04-17 12:07:33 -0300

    add navigation to users

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,7 @@ import {
   SimpleGrid,
   VStack,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
@@ -46,7 +47,11 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
