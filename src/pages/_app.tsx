85919247ae694b8bf88a14591d0691d20b292c19
Natanael Lima <taelima1997@gmail.com>
2021-04-24 08:53:22 -0300

    add config react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { theme } from '../styles/theme';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
@@ -9,13 +11,18 @@ import { makeServer } from '../services/mirage';
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
+
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 export default MyApp;
