6dabb093b28489f861477094b531478d408081c6
Natanael Lima <taelima1997@gmail.com>
2021-04-24 09:33:42 -0300

    change user header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -9,16 +9,16 @@ export function Profile({ showProfileData }: ProfileProps) {
     <Flex align="center">
       {showProfileData && (
         <Box mr="4" textAlign="right">
-          <Text>Diego Fernandes</Text>
+          <Text>Natanael Lima</Text>
           <Text color="gray.300" fontSize="small">
-            diego.schell.f@gmail.com
+            taelima1997@gmail.com
           </Text>
         </Box>
       )}
       <Avatar
         size="md"
-        name="Diego Fernandes"
-        src="https://github.com/diego3g.png"
+        name="Natanael Lima"
+        src="https://github.com/Natanaelvich.png"
       />
     </Flex>
   );
