e225eb6b301e77dc6245048c5211915b77f76dea
Natanael Lima <taelima1997@gmail.com>
2021-04-24 14:31:42 -0300

    add refetch feedback

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,7 +25,7 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     'users',
     async () => {
       const response = await fetch('http://localhost:3000/api/users');
@@ -66,6 +66,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
