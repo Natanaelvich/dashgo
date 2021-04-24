06bf94fa080c9c0950337c754734f3323660af5c
Natanael Lima <taelima1997@gmail.com>
2021-04-24 08:51:12 -0300

    change useefect

--- src/contexts/SidebarDrawerContext.tsx
+++ src/contexts/SidebarDrawerContext.tsx
@@ -18,7 +18,7 @@ export function SidebarDrawerProvider({
 
   useEffect(() => {
     disclosure.onClose();
-  }, [router.asPath, disclosure]);
+  }, [router.asPath]);
 
   return (
     <SidebarDrawerContext.Provider value={disclosure}>
