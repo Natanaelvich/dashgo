0d3141338ee63e7d163943041161eda3aab9aada
Natanael Lima <taelima1997@gmail.com>
2021-04-25 10:14:53 -0300

    update parameter type

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -38,7 +38,7 @@ export default function UserList() {
     lg: true,
   });
 
-  async function handlePrefetchUser(userId: number) {
+  async function handlePrefetchUser(userId: string) {
     await queryClient.prefetchQuery(
       ['user', userId],
       async () => {
@@ -110,7 +110,7 @@ export default function UserList() {
                             <Link
                               color="purple.400"
                               onMouseEnter={() => {
-                                handlePrefetchUser(Number(user.id));
+                                handlePrefetchUser(user.id);
                               }}
                             >
                               <Text fontWeight="bold">{user.name}</Text>
