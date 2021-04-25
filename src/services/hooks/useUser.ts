b28311b7dbc74c0e7055be4509f27ba1828f33b6
Natanael Lima <taelima1997@gmail.com>
2021-04-25 10:45:41 -0300

    change options to optional

--- src/services/hooks/useUser.ts
+++ src/services/hooks/useUser.ts
@@ -41,7 +41,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   };
 }
 
-export function useUsers(page: number, options: UseQueryOptions) {
+export function useUsers(page: number, options?: UseQueryOptions) {
   return useQuery(['users', page], () => getUsers(page), {
     staleTime: 1000 * 60 * 10, // 10 minutes
     ...options,
