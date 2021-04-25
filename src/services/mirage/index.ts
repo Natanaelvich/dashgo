974b1d22634b6179b243958c1bf526b7a3c5853b
Natanael Lima <taelima1997@gmail.com>
2021-04-25 09:47:30 -0300

    update get users

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { createServer, Factory, Model } from 'miragejs';
 import faker from 'faker';
 
@@ -27,15 +28,29 @@ export function makeServer() {
       }),
     },
 
-    seeds(server) {
-      server.createList('user', 5);
+    seeds(serverCreated) {
+      serverCreated.createList('user', 200);
     },
 
     routes() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd,
+        );
+
+        return new Response(200, { 'x-total-count': String(total) }, { users });
+      });
       this.post('/users');
 
       this.namespace = '';
