02ad3b6c600b611182041dd2383582dc27357b10
Natanael Lima <taelima1997@gmail.com>
2021-04-25 10:08:26 -0300

    update with service useQuery

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { theme } from '../styles/theme';
@@ -8,12 +8,11 @@ import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
